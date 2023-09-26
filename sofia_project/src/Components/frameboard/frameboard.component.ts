6fc6cc931f8317634a97a33fd9cdd3f789b8d9dc
SkyWolfxNelson <77088127+SkyWolfxNelson@users.noreply.github.com>
2023-09-26 02:03:07 -0500

    frameboard

--- sofia_project/src/Components/frameboard/frameboard.component.ts
+++ sofia_project/src/Components/frameboard/frameboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-frameboard',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frameboard.component.scss']
 })
 export class FrameboardComponent implements OnInit {
-
+  name = 'World';
   constructor() { }
 
+  @HostListener('window:resize', ['$event'])
+  changeNavHeight() {
+    const header = document.getElementById('header')
+    const navbar = document.getElementById('navbar')
+    const content = document.getElementById('content')
+    if (header && navbar) {
+      navbar.style.height = window.innerHeight - header.offsetHeight + 'px'
+    }
+    if (header && content) {
+      content.style.height = window.innerHeight - header.offsetHeight + 'px'
+    }
+  }
+
   ngOnInit(): void {
+    this.changeNavHeight()
   }
 
 }
