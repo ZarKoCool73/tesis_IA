a2eb1df6a27a534f933f37d235b63076a6760c3f
ZarKoCool73 <dgonzales.dg7@gmail.com>
2023-09-28 09:43:14 -0500

    Formulario de Login

--- sofia_project/src/app/management_user/sign-in/sign-in.component.ts
+++ sofia_project/src/app/management_user/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  msg = 'Buenos días'
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.updateGreeting();
+  }
+
+  updateGreeting() {
+    const now = new Date();
+    const hour = now.getHours();
+
+    if (hour >= 5 && hour < 12) {
+      this.msg = 'Buenos días';
+    } else if (hour >= 12 && hour < 18) {
+      this.msg = 'Buenas tardes';
+    } else {
+      this.msg = 'Buenas noches';
+    }
   }
 
 }
