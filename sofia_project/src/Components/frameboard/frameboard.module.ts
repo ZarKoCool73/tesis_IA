fc47b134606753ad1e131ab4c468126600e1fe96
ZarKoCool73 <dgonzales.dg7@gmail.com>
2023-09-29 17:10:21 -0500

    Agregar obtención del video

--- sofia_project/src/Components/frameboard/frameboard.module.ts
+++ sofia_project/src/Components/frameboard/frameboard.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
 import {FrameboardComponent} from "./frameboard.component";
 import {CommonModule} from "@angular/common";
 import {RouterLinkWithHref, RouterOutlet} from "@angular/router";
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     FrameboardComponent
   ],
-    imports: [
-        CommonModule,
-        RouterOutlet,
-        RouterLinkWithHref,
-    ],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLinkWithHref,
+    HttpClientModule
+  ],
   exports: [
     FrameboardComponent
   ],
   providers: [],
   bootstrap: []
 })
-export class FrameboardModule { }
+export class FrameboardModule {
+}
