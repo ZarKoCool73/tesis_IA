4b11e41ef5d3dd35f7c1a052e1a05caf7571722f
SkyWolfxNelson <77088127+SkyWolfxNelson@users.noreply.github.com>
2023-09-29 17:32:47 -0500

    password recovery

--- sofia_project/src/app/management_user/password-recovery/password-recovery.component.ts
+++ sofia_project/src/app/management_user/password-recovery/password-recovery.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {map, Observable, startWith} from "rxjs";
+import {UtilsService} from "../../../services/utils.service";
 
 @Component({
   selector: 'app-password-recovery',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordRecoveryComponent implements OnInit {
 
-  constructor() { }
+  secondFormGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    repeated_password: new FormControl('', Validators.required),
+  }, this._utils.repeatedPassword)
+  thirdFormGroup = new FormGroup({
+    fquestion: new FormControl('', Validators.required),
+    squestion: new FormControl('', Validators.required),
+    tquestion: new FormControl('', Validators.required),
 
-  ngOnInit(): void {
+  })
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]> | undefined;
+
+  constructor(private _utils: UtilsService) {
+  }
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value: any) => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
 }
