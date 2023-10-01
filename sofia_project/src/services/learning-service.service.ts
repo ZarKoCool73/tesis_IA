e0dfd4fb469cf60efcf3b4cf5667534dc3cb07a9
ZarKoCool73 <dgonzales.dg7@gmail.com>
2023-09-30 20:04:43 -0500

    Actualizacion de video

--- sofia_project/src/services/learning-service.service.ts
+++ sofia_project/src/services/learning-service.service.ts
@@ -3,40 +3,42 @@ import {HttpClient} from '@angular/common/http';
 import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class LearningServiceService {
-    private apiUrl = 'http://localhost:5000/api/video'; // Reemplaza con la URL de tu API de Flask
-    private ventanaEmergenteAbiertaSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private apiUrl = 'http://localhost:5000/api/video'; // Reemplaza con la URL de tu API de Flask
+  private ventanaEmergenteAbiertaSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    /* METODO PARA QUE MUESTRE LA VENTANA EMERGENTE */
-    abrirVentanaEmergente(): void {
-        const popup = window.open(this.apiUrl, '_blank', 'width=800,height=600');
-        if (!popup) {
-            console.error('No se pudo abrir la ventana emergente. Asegúrate de que los bloqueadores de ventanas emergentes estén desactivados.');
-        } else {
-            console.log('pópu', popup)
-            this.ventanaEmergenteAbiertaSubject.next(true);
-            popup.onload = () => {
-                // Resto del código
-                popup.onclose = () => {
-                    this.ventanaEmergenteAbiertaSubject.next(false);
-                };
-            };
+  /* METODO PARA QUE MUESTRE LA VENTANA EMERGENTE */
+  abrirVentanaEmergente(): void {
+    const width = 635;
+    const height = 480;
+
+    const left = (window.innerWidth - width) / 2;
+    const top = (window.innerHeight - height) / 2;
+
+    const popup = window.open(this.apiUrl, '_blank', `width=${width},height=${height},left=${left},top=${top}`);
+
+    console.log(popup)
+    // Agrega un event listener para escuchar mensajes de la ventana emergente
+    window.addEventListener('message', (event) => {
+      // Verifica si el mensaje proviene de la ventana emergente
+      if (event.source === popup) {
+        // Verifica si el mensaje indica que la ventana emergente se ha cerrado
+        if (event.data === 'ventanaCerrada') {
+          // Realiza acciones adicionales aquí si es necesario
+          console.log('La ventana emergente se ha cerrado.');
         }
-    }
+      }
+    });
+  }
 
-    /* SERVICIO PARA LA OBTENCIÓN DEL API DE FLASK */
-    obtenerFlujoVideo(): Observable<ArrayBuffer> {
-        return this.http.get(this.apiUrl, {responseType: 'arraybuffer'});
-    }
-
-    /* MÉTODO PARA VERIFICAR EL ESTADO DE LA VENTANA EMERGENTE */
-    isVentanaEmergenteAbierta(): Observable<boolean> {
-        return this.ventanaEmergenteAbiertaSubject.asObservable();
-    }
+  /* MÉTODO PARA VERIFICAR EL ESTADO DE LA VENTANA EMERGENTE */
+  isVentanaEmergenteAbierta(): Observable<boolean> {
+    return this.ventanaEmergenteAbiertaSubject.asObservable();
+  }
 
 }
