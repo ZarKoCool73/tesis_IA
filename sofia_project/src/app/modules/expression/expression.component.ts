6ddbbb6a0e629a23634e89173400848eb1553bf1
ZarKoCool73 <dgonzales.dg7@gmail.com>
2023-10-04 17:09:15 -0500

    Actualización de codigo

--- sofia_project/src/app/modules/expression/expression.component.ts
+++ sofia_project/src/app/modules/expression/expression.component.ts
@@ -5,62 +5,62 @@ import Swal from "sweetalert2";
 import {ResourceServiceService} from "../../../services/resource-service.service";
 
 @Component({
-  selector: 'app-expression',
-  templateUrl: './expression.component.html',
-  styleUrls: ['./expression.component.scss']
+    selector: 'app-expression',
+    templateUrl: './expression.component.html',
+    styleUrls: ['./expression.component.scss']
 })
 export class ExpressionComponent implements OnInit {
 
-  link = ''
-  categoryExpression: any;
-  category = 'expression'
+    link = ''
+    categoryExpression: any;
+    fullPath: any;
+    category = 'expression'
 
-  constructor(
-    private _activatedRoute: ActivatedRoute,
-    private _servicesResource: ResourceServiceService) {
+    constructor(
+        private _activatedRoute: ActivatedRoute,
+        private _servicesResource: ResourceServiceService) {
 
-  }
+    }
 
-  ngOnInit(): void {
-    this._activatedRoute.params.subscribe((params: any) => {
-      switch (params.type) {
-        case 'abc':
-          this.link = 'Abecedario';
-          this.loadDetail('abc')
-          break;
-        case  'common-expressions':
-          this.link = 'Expresiones comunes';
-          this.loadDetail('common-expressions')
-          break;
-        case   'numbers':
-          this.link = 'Números';
-          this.loadDetail('numbers')
-          break;
-        case  'colors':
-          this.link = 'Colores';
-          this.loadDetail('colors')
-          break;
-      }
-    })
-  }
+    ngOnInit(): void {
+        this._activatedRoute.params.subscribe((params: any) => {
+            switch (params.type) {
+                case 'abc':
+                    this.link = 'Abecedario';
+                    this.loadDetail('abc')
+                    break;
+                case  'common-expressions':
+                    this.link = 'Expresiones comunes';
+                    this.loadDetail('common-expressions')
+                    break;
+                case   'numbers':
+                    this.link = 'Números';
+                    this.loadDetail('numbers')
+                    break;
+                case  'colors':
+                    this.link = 'Colores';
+                    this.loadDetail('colors')
+                    break;
+            }
+        })
+    }
 
-  loadDetail(id: any) {
-    this._servicesResource.getCategory(id).subscribe((res: any) => {
-      if (res.state == 1) {
-        console.log('res', res)
-        this.categoryExpression = res.resources
-      }
-    }, (error: HttpErrorResponse) => {
-      Swal.close();
-      if (error.error.state == 0) {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: error.error.message,
-          confirmButtonColor: '#ff3600',
-        });
-      }
-    })
-  }
-
+    loadDetail(id: any) {
+        this._servicesResource.getCategory(id).subscribe((res: any) => {
+            if (res.state == 1) {
+                console.log('res', res)
+                this.categoryExpression = res.resources
+            }
+        }, (error: HttpErrorResponse) => {
+            Swal.close();
+            if (error.error.state == 0) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: error.error.message,
+                    confirmButtonColor: '#ff3600',
+                });
+            }
+        })
+    }
 }
