c08827ba0700bf31281bd3929aa163d3454a181a
ZarKoCool73 <dgonzales.dg7@gmail.com>
2023-10-06 16:44:27 -0500

    actualziaci´+on de contraseña

--- sofia_project/src/app/management_user/password-recovery/password-recovery.component.ts
+++ sofia_project/src/app/management_user/password-recovery/password-recovery.component.ts
@@ -6,117 +6,146 @@ import {UserServiceService} from "../../../services/user-service.service";
 import {HttpErrorResponse} from "@angular/common/http";
 import Swal from "sweetalert2";
 import {MatStepper} from '@angular/material/stepper';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-password-recovery',
-  templateUrl: './password-recovery.component.html',
-  styleUrls: ['./password-recovery.component.scss'],
-  viewProviders: [{provide: MatStepper}]
+    selector: 'app-password-recovery',
+    templateUrl: './password-recovery.component.html',
+    styleUrls: ['./password-recovery.component.scss'],
+    viewProviders: [{provide: MatStepper}]
 })
 export class PasswordRecoveryComponent implements OnInit {
 
-  secondFormGroup = new FormGroup({
-    email: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
-    repeated_password: new FormControl('', Validators.required),
-  }, this._utils.repeatedPassword)
+    secondFormGroup = new FormGroup({
+        email: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required),
+        repeated_password: new FormControl('', Validators.required),
+    }, this._utils.repeatedPassword)
 
-  thirdFormGroup = new FormGroup({
-    fquestion: new FormControl('', Validators.required),
-    squestion: new FormControl('', Validators.required),
-    tquestion: new FormControl('', Validators.required),
+    thirdFormGroup = new FormGroup({
+        fquestion: new FormControl('', Validators.required),
+        squestion: new FormControl('', Validators.required),
+        tquestion: new FormControl('', Validators.required),
 
-  })
-  myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
-  filteredOptions: Observable<string[]> | undefined;
-  bandera = false;
-  message = ''
+    })
+    myControl = new FormControl();
+    options: string[] = ['One', 'Two', 'Three'];
+    filteredOptions: Observable<string[]> | undefined;
+    bandera = false;
+    message = ''
 
-  constructor(
-    private _utils: UtilsService,
-    private _userService: UserServiceService,
-    private _stepper: MatStepper) {
-  }
-
-  ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map((value: any) => this._filter(value))
-    );
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
-  }
-
-  obtenerData(event: any) {
-    const values = this.secondFormGroup.getRawValue()
-    const body = values.email
-    console.log('body', body)
-    if (body != null) {
-      this._userService.searchEmail(body).subscribe((res: any) => {
-        if (res.state == 1) {
-          event.next()
-        }
-      }, (error: HttpErrorResponse) => {
-        Swal.close();
-        if (error.error.state == 0) {
-          Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: error.error.message,
-            confirmButtonColor: '#ff3600',
-          });
-        }
-      })
+    constructor(
+        private _utils: UtilsService,
+        private _userService: UserServiceService,
+        private _router: Router,
+        private _stepper: MatStepper) {
     }
-  }
 
-  dataCompleta() {
-    const values = this.secondFormGroup.getRawValue()
-    const values1 = this.thirdFormGroup.getRawValue()
-    const email = values.email;
-    const firstQuestion = values1.fquestion;
-    const secondQuestion = values1.squestion;
-    const thirdQuestion = values1.tquestion;
-
-    this._userService.verifyAnswers(email, firstQuestion, secondQuestion, thirdQuestion)
-      .subscribe(
-        (res: any) => {
-          if (res.state === 1) {
-            // La respuesta de seguridad es correcta, haz algo aquí
-          } else {
-            // La respuesta de seguridad es incorrecta, haz algo aquí
-          }
-        }, (error: HttpErrorResponse) => {
-          // Maneja errores aquí
-        }
-      );
-  }
-
-  validarContrasenaFuerte(contrasena: any): { valido: boolean, mensaje: string } {
-    console.log('con', contrasena);
-    let mensaje = '';
-    let valido = true;
-    if (contrasena.length < 8) {
-      mensaje = 'La contraseña debe tener al menos 8 caracteres de longitud.';
-      valido = false;
-    } else if (!/[A-Z]/.test(contrasena) || !/[a-z]/.test(contrasena) || !/\d/.test(contrasena)) {
-      mensaje = 'La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un dígito.';
-      valido = false;
-    } else {
-      console.log('ab', contrasena);
-      this.bandera = true; // Contraseña fuerte
+    ngOnInit() {
+        this.filteredOptions = this.myControl.valueChanges.pipe(
+            startWith(''),
+            map((value: any) => this._filter(value))
+        );
     }
-    return {valido, mensaje};
-  }
 
-  validatePassword() {
-    const resultado = this.validarContrasenaFuerte(this.secondFormGroup.value.password);
-    this.bandera = resultado.valido;
-    this.message = resultado.mensaje; // Establece el mensaje de validación en la variable message.
-  }
+    private _filter(value: string): string[] {
+        const filterValue = value.toLowerCase();
+
+        return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+    }
+
+    obtenerData(event: any) {
+        const values = this.secondFormGroup.getRawValue()
+        const body = values.email
+        console.log('body', body)
+        if (body != null) {
+            this._userService.searchEmail(body).subscribe((res: any) => {
+                if (res.state == 1) {
+                    event.next()
+                }
+            }, (error: HttpErrorResponse) => {
+                Swal.close();
+                if (error.error.state == 0) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: error.error.message,
+                        confirmButtonColor: '#ff3600',
+                    });
+                }
+            })
+        }
+    }
+
+    dataCompleta() {
+        const values = this.secondFormGroup.getRawValue()
+        const email = values.email;
+        const values1 = this.thirdFormGroup.getRawValue()
+        const firstQuestion = values1.fquestion;
+        const secondQuestion = values1.squestion;
+        const thirdQuestion = values1.tquestion;
+
+        this._userService.verifyAnswers(email, firstQuestion, secondQuestion, thirdQuestion)
+            .subscribe(
+                (res: any) => {
+                    if (res.state === 1) {
+                        this.reestablecerContraseña()
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error',
+                            text: 'Una de las preguntas es incorrecta',
+                            confirmButtonColor: '#ff3600',
+                        });
+                    }
+                }, (error: HttpErrorResponse) => {
+                }
+            );
+    }
+
+    reestablecerContraseña() {
+        const values = this.secondFormGroup.getRawValue()
+        const email = values.email;
+        const password = values.password;
+        this._userService.resetPassword(email, password).subscribe(
+            (response: any) => {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Éxito',
+                    text: 'Su contraseña ha sido actualizada',
+                    confirmButtonColor:'#11e38a'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        this._router.navigate(['/account']);
+                    }
+                });
+            },
+            (error: any) => {
+                console.error('Error al restablecer la contraseña', error);
+            }
+        );
+    }
+
+    validarContrasenaFuerte(contrasena: any): { valido: boolean, mensaje: string } {
+        console.log('con', contrasena);
+        let mensaje = '';
+        let valido = true;
+        if (contrasena.length < 8) {
+            mensaje = 'La contraseña debe tener al menos 8 caracteres de longitud.';
+            valido = false;
+        } else if (!/[A-Z]/.test(contrasena) || !/[a-z]/.test(contrasena) || !/\d/.test(contrasena)) {
+            mensaje = 'La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un dígito.';
+            valido = false;
+        } else {
+            console.log('ab', contrasena);
+            this.bandera = true; // Contraseña fuerte
+        }
+        return {valido, mensaje};
+    }
+
+    validatePassword() {
+        const resultado = this.validarContrasenaFuerte(this.secondFormGroup.value.password);
+        this.bandera = resultado.valido;
+        this.message = resultado.mensaje; // Establece el mensaje de validación en la variable message.
+    }
 }
--- sofia_project/src/services/user-service.service.ts
+++ sofia_project/src/services/user-service.service.ts
@@ -3,57 +3,56 @@ import {HttpClient} from "@angular/common/http";
 import {CommonServices} from "../app/CommonServices";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserServiceService {
 
-  private baseUrl = ''
+    private baseUrl = ''
 
-  constructor(private _http: HttpClient, private _common: CommonServices) {
-    this.baseUrl = this._common.BASE_URL + ''
-  }
+    constructor(private _http: HttpClient, private _common: CommonServices) {
+        this.baseUrl = this._common.BASE_URL + ''
+    }
 
-  getUser(id: string) {
-    const headers = {params: {}}
-    return this._http.get(`${this.baseUrl}users/${id}`, headers)
-  }
+    getUser(id: string) {
+        const headers = {params: {}}
+        return this._http.get(`${this.baseUrl}users/${id}`, headers)
+    }
 
-  saveUser(body: {
-    name: string | null,
-    lastname: string | null,
-    age: string | null,
-    email: string | null,
-    studentCode: string | null,
-    firstQuestion: string | null,
-    secondQuestion: string | null,
-    thirdQuestion: string | null,
-    password: string | null
-  }) {
-    return this._http.post(`${this.baseUrl}users`, body)
-  }
+    saveUser(body: {
+        name: string | null,
+        lastname: string | null,
+        age: string | null,
+        email: string | null,
+        studentCode: string | null,
+        firstQuestion: string | null,
+        secondQuestion: string | null,
+        thirdQuestion: string | null,
+        password: string | null
+    }) {
+        return this._http.post(`${this.baseUrl}users`, body)
+    }
 
-  login(body: { password: string | null; email: string | null }) {
-    return this._http.post(`${this.baseUrl}login`, body)
-  }
+    login(body: { password: string | null; email: string | null }) {
+        return this._http.post(`${this.baseUrl}login`, body)
+    }
 
-  verifyAnswers(email: string | null, firstQuestion: string | null, secondQuestion: string | null, thirdQuestion: string | null) {
-    const body = {
-      email: email,
-      firstQuestion: firstQuestion,
-      secondQuestion: secondQuestion,
-      thirdQuestion: thirdQuestion
-    };
+    verifyAnswers(email: string | null, firstQuestion: string | null, secondQuestion: string | null, thirdQuestion: string | null) {
+        const body = {
+            email: email,
+            firstQuestion: firstQuestion,
+            secondQuestion: secondQuestion,
+            thirdQuestion: thirdQuestion
+        };
 
-    return this._http.post(`${this.baseUrl}/users/check-security-answer`, body);
-  }
+        return this._http.post(`${this.baseUrl}/users/check-security-answer`, body);
+    }
 
-  searchEmail(email: string) {
-    return this._http.post<any>(`${this.baseUrl}users/${email}`, {email});
-  }
+    searchEmail(email: string) {
+        return this._http.post<any>(`${this.baseUrl}users/${email}`, {email});
+    }
 
-  changePassword(email: string, body: {
-    password: string
-  }) {
-    return this._http.put(`${this.baseUrl}users/${email}`, body)
-  }
+    resetPassword(email: string | null, newPassword: string | null) {
+        const body = {email, newPassword};
+        return this._http.post(`${this.baseUrl}/users/reset-password`, body);
+    }
 }
