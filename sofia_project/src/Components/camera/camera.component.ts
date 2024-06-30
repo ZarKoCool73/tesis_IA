a1d46cace9aae08831306389ffc89e6a48135fc1
ZarKoCool73 <dgonzales.dg7@gmail.com>
2024-06-30 18:45:29 -0500

    Agregar icono de camara y vista

--- sofia_project/src/Components/camera/camera.component.ts
+++ sofia_project/src/Components/camera/camera.component.ts
@@ -15,19 +15,29 @@ import {EntityService} from "../../services/entity.service";
 export class CameraComponent implements OnInit, AfterViewInit {
 
   @Input() dataCollection: any[] = []
-  @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
+  //@ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
+
+  @ViewChild('localVideo') localVideo!: ElementRef<HTMLVideoElement>;
+  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
+
+  title = '';
+  resultTitle = '';
+  resultContent = '';
+  resultAccuracy = '';
 
   ngAfterViewInit(): void {
+    this.setupWebRTC();
+    this.updateTitle();
   }
 
   ngOnInit(): void {
-    this.startCamera()
+    //this.startCamera()
   }
 
   constructor() {
   }
 
-  startCamera() {
+  /*startCamera() {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({video: true})
         .then(stream => {
@@ -51,5 +61,63 @@ export class CameraComponent implements OnInit, AfterViewInit {
         this.videoElement.nativeElement.srcObject = null;
       }
     }
+  }*/
+
+  updateTitle() {
+    if (window.location.href.includes('expressions')) {
+      this.title = 'Módulo de expresión';
+    } else {
+      this.title = 'Módulo de comprensión';
+    }
+  }
+
+  async setupWebRTC() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+      this.localVideo.nativeElement.srcObject = stream;
+    } catch (error) {
+      console.error('Error en la configuración de WebRTC:', error);
+      Swal.fire('Error', 'No se pudo acceder a la cámara', 'error');
+    }
+  }
+
+  captureAndProcessImage() {
+    const canvas = this.canvas.nativeElement;
+    const context = canvas.getContext('2d');
+    canvas.width = this.localVideo.nativeElement.videoWidth;
+    canvas.height = this.localVideo.nativeElement.videoHeight;
+    // @ts-ignore
+    context.drawImage(this.localVideo.nativeElement, 0, 0, canvas.width, canvas.height);
+
+    const imgData = canvas.toDataURL('image/jpeg');
+    const expressions = window.location.href.includes('expressions');
+
+    fetch('http://services-ia.onrender.com/expressions?camera_id=8', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        imageData: imgData,
+        Expressions: expressions
+      })
+    }).then(res => res.json().then(data => {
+      if (!res.ok) {
+        throw new Error(data.error || 'Network response was not ok');
+      }
+      return data;
+    })).then(data => {
+      const accuracyPercentage = data.accuracy ? `Precisión: ${data.accuracy.toFixed(2)}%` : '';
+      if (data.accuracy > 60) {
+        this.resultTitle = 'Interpretación del signo';
+        this.resultContent = `${data.sign}`;
+        this.resultAccuracy = accuracyPercentage;
+      }
+    }).catch(error => {
+      console.log('error', error);
+      this.resultTitle = 'Interpretación del signo';
+      this.resultContent = '-';
+      this.resultAccuracy = `${error.message}`;
+    });
   }
 }
